7aaa1b44a8970415cba69747ca461b154f5a91df
Jaydeep Das <jaydeepjd.8914@gmail.com>
2024-08-27 15:19:19 +0530

    added skeleton for firebase auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,22 @@
+"use client";
 import React from 'react'
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from "../utils/firebase"
 
-function page() {
+export default function Dashboard() {
+  const [user, loading, error] = useAuthState(auth);
+  if (error) {
+    return <div>There was some error. Please contact support</div>
+  }
+  if (loading) {
+    return (
+      <div>
+        Dashboard Page.
+        Loading User...
+      </div>
+    )
+  }
   return (
-    <div>page</div>
+    <div>Dashboard Page. Signed in as {user?.displayName} </div>
   )
 }
-
-export default page
