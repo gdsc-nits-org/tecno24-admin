c4e706782df9e5c08db7ad3afe2a039e156ad2d9
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-08-28 23:53:53 +0530

    add api

--- src/app/event/[id]/page.tsx
+++ src/app/event/[id]/page.tsx
@@ -13,37 +13,32 @@ import { Button } from "~/components/ui/button";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { env } from "~/env";
+export const runtime = "edge";
+
+interface TeamMember {
+    id: string;
+    firebaseId: string;
+    firstName: string;
+    lastName: string;
+    middleName?: string;
+    username: string;
+    email: string;
+    collegeName: string;
+    registrationId: string;
+    phoneNumber: string;
+}
+interface Team {
+    id: string;
+    teamName: string;
+    registrationStatus: string;
+    members: TeamMember[];
+    extraInformation: Record<string, any>[];
+    eventId: string;
+}
 
 interface EventParams {
     id: string;
 }
-
-interface UserData {
-    avatar_url: string;
-    login: string;
-}
-
-export const runtime = "edge";
-const tablesData = [
-    {
-        id: 1,
-        caption: 'Invoices for August',
-        rows: [
-            { invoice: 'INV001', status: 'Paid', method: 'Credit Card', amount: '$250.00' },
-            { invoice: 'INV002', status: 'Pending', method: 'PayPal', amount: '$75.00' },
-        ],
-    },
-    {
-        id: 2,
-        caption: 'Invoices for September',
-        rows: [
-            { invoice: 'INV003', status: 'Paid', method: 'Bank Transfer', amount: '$400.00' },
-            { invoice: 'INV004', status: 'Overdue', method: 'Credit Card', amount: '$150.00' },
-        ],
-    },
-];
-
-// Convert JSON to CSV
 const jsonToCSV = (data: Record<string, string | number>[]): string => {
     if (data.length === 0) return '';
     const headers = data[0] ? Object.keys(data[0]) : [];
@@ -61,103 +56,108 @@ const jsonToCSV = (data: Record<string, string | number>[]): string => {
     return csvRows.join('\n');
 };
 
-const downloadCSV = async () => {
-    try {
-        const response = await fetch('https://api.github.com/users'); // Adjust the API endpoint
-        if (!response.ok) {
-            throw new Error('Network response was not ok');
-        }
-        const jsonData: UserData[] = await response.json() as UserData[];;
-        const filteredData = jsonData.map((item: UserData) => ({
-            field1: item.avatar_url,
-            field2: item.login,
-        }));
-        const csvData = jsonToCSV(filteredData);
-        const blob = new Blob([csvData], { type: 'text/csv' });
-        const url = window.URL.createObjectURL(blob);
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'teams.csv');
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    } catch (error) {
-        console.error('Error downloading the CSV:', error);
-    }
-};
-interface TeamMember {
-    id: string;
-    firebaseId: string;
-    firstName: string;
-    lastName: string;
-    middleName?: string;
-    username: string;
-    email: string;
-    collegeName: string;
-    registrationId: string;
-    phoneNumber: string;
-}
-interface Team {
-    id: string;
-    teamName: string;
-    registrationStatus: string; 
-    members: TeamMember[];
-    extraInformation: Record<string, any>[];
-    eventId: string;
-}
-
-interface EventParams {
-    eventId: string;
-    eventName: string;
-}
-
 const Event = ({ params }: { params: EventParams }) => {
+    const [name, setName] = useState("");
     const [teams, setTeams] = useState<Team[]>([]);
-    const { eventId, eventName } = params;
-
     useEffect(() => {
+        // console.log(params.id)
         const fetchTeams = async () => {
             try {
-                const response = await axios.get(`${env.NEXT_PUBLIC_API_URL}/api/team/event/${eventId}/registered_teams`);
-                setTeams(response.data);
+                const name = await axios.get(`${env.NEXT_PUBLIC_API_URL}/api/event/${params.id}`)
+                const response = await axios.get(`${env.NEXT_PUBLIC_API_URL}/api/team/event/${params.id}/registered_teams`,
+                    {
+                        headers: {
+                            'Authorization': `Bearer 1000000`
+                        }
+                    })
+                console.log(response.data.msg)
+                setTeams(response.data.msg);
+                setName(name.data.msg.name);
             } catch (error) {
                 console.error("Error fetching team data:", error);
             }
         };
 
         fetchTeams();
-    }, [eventId]);
+    }, []);
+
+    const downloadCSV = () => {
+        //CSV headers
+        const headers = [{
+            "Sl No.": "Sl No.",
+            "Team Name": "Team Name",
+            "Name": "Name",
+            "Email": "Email",
+            "Team Member Phone": "Team Member Phone",
+            "College Name": "College Name"
+        }];
+
+        const filteredData = teams.flatMap((team, teamIndex) => {
+            // Add a row for the team index
+            const teamHeader = [{
+                "Sl No.": `Team ${teamIndex + 1}`,
+                "Team Name": team.teamName,
+                "Name": '',
+                "Email": '',
+                "Team Member Phone": '',
+                "College Name": ''
+            }];
+
+            // Add members of the team
+            const teamMembers = team.members.map((member, memberIndex) => ({
+                "Sl No.": memberIndex + 1,
+                "Team Name": '',
+                "Name": `${member.firstName} ${member.middleName ? member.middleName + ' ' : ''}${member.lastName}`,
+                "Email": member.email,
+                "Team Member Phone": member.phoneNumber,
+                "College Name": member.collegeName
+            }));
+
+            // Return the combined array of the team header, members, and  empty rows for spacing
+            return [...teamHeader, ...teamMembers, {}, {}];
+        });
+
+        // Combine the headers with the data
+        const csvData = jsonToCSV([...headers, ...filteredData]);
+        const blob = new Blob([csvData], { type: 'text/csv' });
+        const url = window.URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `${name}`);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    };
+
 
     return (
         <main className="flex flex-col justify-center items-center h-screen p-4">
-        <div className="flex flex-row justify-center items-center w-full text-4xl font-mono font-bold uppercase py-8 my-10 text-center">
-            {eventName}
-        </div>
-        {teams.map((team, teamIndex) => (
-            <div key={team.id} className="w-full max-w-4xl p-4 mb-6">
-                <Table className="w-full shadow-md rounded-lg">
-                    <TableCaption className="text-left py-2 font-semibold">{`Team ${teamIndex + 1}: ${team.teamName}`}</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                            <TableHead className="w-[50px]">Sl No.</TableHead>
-                            <TableHead>Team Name</TableHead>
-                            <TableHead>Team Member Name</TableHead>
-                            <TableHead>Registration Status</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {team.members.map((member, memberIndex) => (
-                            <TableRow key={member.id}>
-                                <TableCell className="font-medium">{memberIndex + 1}</TableCell>
-                                <TableCell>{team.teamName}</TableCell>
-                                <TableCell>{`${member.firstName} ${member.middleName ? member.middleName + ' ' : ''}${member.lastName}`}</TableCell>
-                                <TableCell>{team.registrationStatus}</TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
+            <div className="flex flex-row justify-center items-center w-full text-4xl font-mono font-bold uppercase py-8 my-10 text-center">
+                {name}
             </div>
-        ))}
+            {teams?.map((team, teamIndex) => (
+                <div key={team.id} className="w-full max-w-4xl p-4 mb-6">
+                    <Table className="w-full shadow-md rounded-lg">
+                        <TableCaption className="text-left py-2 font-semibold">{`Team ${teamIndex + 1}: ${team.teamName}`}</TableCaption>
+                        <TableHeader>
+                            <TableRow>
+                                <TableHead className="w-[50px]">Sl No.</TableHead>
+                                <TableHead>Team Name</TableHead>
+                                <TableHead>Team Member Name</TableHead>
+                            </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                            {team.members.map((member, memberIndex) => (
+                                <TableRow key={member.id}>
+                                    <TableCell className="font-medium">{memberIndex + 1}</TableCell>
+                                    <TableCell>{team.teamName}</TableCell>
+                                    <TableCell>{`${member.firstName} ${member.middleName ? member.middleName + ' ' : ''}${member.lastName}`}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </div>
+            ))}
             <Button onClick={downloadCSV} className="mt-6 text-lg font-mono font-bold" variant="outline">
                 Download
             </Button>
