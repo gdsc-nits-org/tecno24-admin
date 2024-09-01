32afbd8abfc621c232134534dd39fcf46a22fa1b
RISHI RAJ SAHA <rishir_ug_22@ece.nits.ac.in>
2024-09-01 19:39:05 +0530

    bug fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,13 @@ import Link from "next/link";
 import { useRouter } from 'next/navigation'; 
 import { Button } from '~/components/ui/button';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from './utils/firebase';
 import { useEffect } from 'react';
 
 export default function HomePage() {
   const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+  const [user1, loading1, error1] = useAuthState(auth);
   const router = useRouter();
 
   useEffect(() => {
@@ -50,6 +52,7 @@ export default function HomePage() {
       </div>
     );
   }
+  if(!user1)
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
       <h1 className="text-xl my-2">Tecnoesis 2024 Admin Panel. Please Sign In to continue</h1>
@@ -58,4 +61,7 @@ export default function HomePage() {
       </Button>
     </main>
   );
+  else{
+    router.push('/dashboard');
+  }
 }
